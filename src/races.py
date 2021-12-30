df813778ec5afd1e6d7828b0cfa40c105532698d
Steven Biro <steven.biro@mail.utoronto.ca>
2021-12-29 20:27:27 -0500

    Revert "short aliases for in-game done and forfeit commands because many of us have tried these and are surprised to find it doesnt work (#27)"
    This reverts commit c15d0d2933004360094c0765685271cef889bfb7.

--- src/races.py
+++ src/races.py
@@ -262,7 +262,7 @@ class Races(commands.Cog):
         msg = race.getUpdate()
         await ctx.channel.send(msg)
 
-    @commands.command(aliases=["d"])
+    @commands.command()
     @is_race_started()
     @is_runner()
     @commands.check(is_race_room)
@@ -288,7 +288,7 @@ class Races(commands.Cog):
         except KeyError:
             await ctx.channel.send("Key Error in 'undone' command")
 
-    @commands.command(aliases=["ff"])
+    @commands.command()
     @is_race_started()
     @is_runner()
     @commands.check(is_race_room)
